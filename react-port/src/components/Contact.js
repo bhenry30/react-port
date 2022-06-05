919c9fb0c0962eb5e45d762853a8a48fa2871021
Brandon Henry <brandonhenry@Ginas-iMac.lan>
2022-06-05 18:47:29 -0500

    creates Contact component

--- react-port/src/components/Contact.js
+++ react-port/src/components/Contact.js
@@ -1,1 +1,19 @@
 import React from 'react';
+
+function Contact() {
+    return (
+        <div className="card">
+            <h1>Contact</h1>
+            <div className="contact-info" >
+                <h3>Phone: 469.964.4890</h3>
+                <h3>Email: brandonhenry214@gmail.com</h3>
+                <a href="https://www.linkedin.com/in/bhenry40/">LinkedIn</a>
+                <a href="https://github.com/bhenry30">GitHub</a>
+                <a href="https://www.instagram.com/mrbrandonhenry/">Instagram</a>
+        </div>
+        </div>
+
+    )
+}
+
+module.exports = Contact
